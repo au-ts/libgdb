8050ca54ab091aed23bb5bb46d63f81154f73fee
Krishnan Winter <krishnanwinter1@gmail.com>
2025-06-11 11:44:11 +1000

    update libgdb to work with latest child vspace microkit changes
    Signed-off-by: Krishnan Winter <krishnanwinter1@gmail.com>

--- libvspace/vspace.h
+++ libvspace/vspace.h
@@ -2,8 +2,12 @@
 
 #define NUM_DEBUGEES 2
 
-microkit_uint64_t table_metadata[64];
-microkit_uint64_t table_data[0x800 * (NUM_DEBUGEES + 100)];
+typedef struct table_meta_data {
+    uint64_t table_data_base;
+    uint64_t pgd[64];
+} table_metadata_t;
+
+table_metadata_t table_metadata;
 
 uint32_t read_word(uint16_t client, uintptr_t addr, seL4_Word *val, seL4_Word map_addr);
 uint32_t write_word(uint16_t client, uintptr_t addr, seL4_Word val, seL4_Word map_addr);
